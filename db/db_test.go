0e7152e9db58785e42f3ec958c30b8de7f988cbd
damianu <damian@datagentleman.io>
2025-02-10 15:38:36 +0100

    Initial Collection class

--- db/db_test.go
+++ db/db_test.go
@@ -1,11 +1,26 @@
 package db
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestOpen(t *testing.T) {
 	db, _ := Open("./db")
 
 	if db.root != "./db" {
 		t.Errorf("Database root directory was not set")
+	}
+}
+
+func TestCollection(t *testing.T) {
+	db, _ := Open("./db")
+	db.Collection("test")
+
+	// At this point, we only want to check if the proper
+	// directories have been created.
+	_, err := os.Stat("./db/collections/test/1.bucket")
+	if err != nil {
+		t.Errorf("Collection path doesn't exist")
 	}
 }
