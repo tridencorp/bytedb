44591b6f12a186a929f8d9a758f90e674ac0a1c7
damianu <damian@datagentleman.io>
2025-02-10 16:06:27 +0100

    Move Collection to collection file

---
+++ db/collection.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"os"
+	"path/filepath"
+)
+
+type Collection struct {
+	file *os.File
+}
+
+// Open the collection. If it doesn't exist, 
+// create it with default values.
+func (db *DB) Collection(name string) (*Collection, error) {
+	// Build collection path.
+	path := db.root + CollectionsPath + name + "/1.bucket"
+	dir  := filepath.Dir(path)
+
+	// Create directory structure. Do nothing if it already exist.
+	if err := os.MkdirAll(dir, 0755)
+	err != nil {
+		return nil, err
+	}
+
+	// Open collection bucket file.
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	coll := &Collection{file: file}
+	return coll, nil
+}
--- db/db.go
+++ db/db.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"os"
-	"path/filepath"
 )
 
 const (
@@ -15,10 +14,6 @@ type DB struct {
 	root string
 }
 
-type Collection struct {
-	file *os.File
-}
-
 // Open database.
 func Open(path string) (*DB, error) {
 	return &DB{root: path}, nil
@@ -27,27 +22,4 @@ func Open(path string) (*DB, error) {
 // Delete the entire database.
 func (db *DB) Delete() (error) {
 	return os.RemoveAll(db.root)
-}
-
-// Open the collection. If it doesn't exist, 
-// create it with default values.
-func (db *DB) Collection(name string) (*Collection, error) {
-	// Build collection path.
-	path := db.root + CollectionsPath + name + "/1.bucket"
-	dir  := filepath.Dir(path)
-
-	// Create directory structure. Do nothing if it already exist.
-	if err := os.MkdirAll(dir, 0755)
-	err != nil {
-		return nil, err
-	}
-
-	// Open collection bucket file.
-	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
-	if err != nil {
-		return nil, err
-	}
-
-	coll := &Collection{file: file}
-	return coll, nil
 }
