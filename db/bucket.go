673c15b2e708543fd1c6a0c1f56e90b59bb1e503
damianu <damian@datagentleman.io>
2025-02-11 19:32:57 +0100

    Writes to bucket

--- db/bucket.go
+++ db/bucket.go
@@ -1,11 +1,20 @@
 package db
 
-import "os"
+import (
+	"os"
+	"sync/atomic"
+)
 
 type Bucket struct {
 	ID 		uint32
 	Dir   string
+
 	file *os.File
+
+	// We will be using atomic.Add() for each key.
+	// In combination with WriteAt, it should give
+	// us the ultimate concurrent writes.
+	offset atomic.Int64
 }
 
 func OpenBucket(file string) (*Bucket, error) {
@@ -18,4 +27,31 @@ func OpenBucket(file string) (*Bucket, error) {
 	// TODO: Temporary values untill we have proper bucket management.
 	bck := &Bucket{ID:1, Dir: "", file: f}
 	return bck, nil;
+}
+
+// Write data to bucket.
+//
+// TODO: Should buckets know about keys and other
+// types ? Should they operate only on raw bytes ?
+func (bucket *Bucket) Write(data []byte) (int64, int64, error) {
+	// We are adding len to atomic value and then deducting it
+	// from the result, this should give us space for our data.
+	//
+	// TODO: file must be truncated first !!! Make sure that we have
+	// enough space for data. For truncating we can use write mutex 
+	// and try to allocate enough space.
+	off := bucket.offset.Add(int64(len(data)))
+	off -= int64(len(data))
+
+	// We are using WriteAt because, when carefully
+	// handled, it's concurrent-friendly.
+
+	// TODO: handle file truncation here. Make sure that we have 
+	// enough space for offset and data.
+	size, err := bucket.file.WriteAt(data, off)
+	if err != nil {
+		return off, int64(size), err
+	}
+
+	return off, int64(size), nil
 }
--- db/bucket_test.go
+++ db/bucket_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestOpens(t *testing.T) {
+func TestOpenBucket(t *testing.T) {
 	db1, _ := Open("./db")
 	db1.Delete()
 
@@ -13,5 +13,19 @@ func TestOpens(t *testing.T) {
 	bck, _ := OpenBucket(coll.root + "/1.bucket")
 	if bck == nil {
 		t.Errorf("Expected Bucket object, got nil")
+	}
+}
+
+func TestBucketWrite(t *testing.T) {
+	db1, _ := Open("./db")
+	db1.Delete()
+
+	coll, _ := db1.Collection("test")
+	bck,  _ := OpenBucket(coll.root + "/1.bucket")
+
+	data := []byte("value1")
+	_, size, _ := bck.Write(data)
+	if size != int64(len(data)) {
+		t.Errorf("Expected %d bytes to be written, got %d.", int64(len(data)), size)
 	}
 }
