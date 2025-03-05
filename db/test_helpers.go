c4a123a06d3344e589b2d04a06f57c182eb01bd4
damianu <damian@datagentleman.io>
2025-03-05 02:01:18 +0100

    FillCollection test helper

--- db/test_helpers.go
+++ db/test_helpers.go
@@ -1,10 +1,35 @@
 package db
 
+import (
+	"fmt"
+
+	"golang.org/x/exp/rand"
+)
+
 func CreateCollection(name string, keysLimit uint32, sizeLimit int64, bucketsPerDir int32) (*DB, *Collection) {
 	database, _ := Open("./testdb")
 
-	conf := Config{KeysLimit: 2, SizeLimit: 10, BucketsPerDir: 2}
-	coll, _:= database.Collection("test", conf)
+	conf    := Config{KeysLimit: 2, SizeLimit: 10, BucketsPerDir: 2}
+	coll, _ := database.Collection("test", conf)
 
 	return database, coll
+}
+
+// Write the specified number of keys to the collection, with each key having the given size in Bytes.
+func FillCollection(coll *Collection, numOfKeys, sizeOfKey uint32) (written int64, values [][]byte) {
+	for i:=0; i < int(numOfKeys); i++ {
+		val := make([]byte, sizeOfKey)
+
+		// Fill it with random bytes.
+		for i := range val {
+			val[i] = byte(rand.Intn(255))
+		}
+
+		key := fmt.Sprintf("key_%d", i)
+		coll.Set(key, val)
+
+		values = append(values, val)
+	}
+
+	return written, values
 }
