ff3ec33d1321d7607037482d9848c559ee5bcae8
damianu <damian@datagentleman.io>
2025-03-18 00:12:12 +0100

    Apply tests.Assert

--- db/buckets_test.go
+++ db/buckets_test.go
@@ -1,13 +1,13 @@
 package db
 
 import (
+	"bucketdb/tests"
 	"testing"
 )
 
 func TestOpenBuckets(t *testing.T) {
 	conf := Config{2, 1_000_000, 2, 100}
+
 	_, err := OpenBuckets("./test", conf)
-	if err != nil {
-		t.Errorf("Error when opening buckets: %s", err)
-	}
+	tests.Assert(t, err, nil)
 }
