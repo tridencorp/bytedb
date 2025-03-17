d711c454b3eed9433802bbce52c0176869109e7c
damianu <damian@datagentleman.io>
2025-03-17 23:45:57 +0100

    Extract KV to separate file

---
+++ db/kv.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+// KV structure that represents each key/value stored in collection.
+// TODO: Key will be hashed key, uint64.
+type KV struct {
+	key  []byte
+	val  []byte
+}
+
+func NewKV(key string, val []byte) *KV {
+	return &KV{[]byte(key), val}
+}
+
+// Read kv data from bytes.
+func (kv *KV) FromBytes(raw []byte) {
+	buf := bytes.NewBuffer(raw)
+
+	// Decode size and data. We don't need size at this point
+	// so we can just skip next 4 bytes (since size is uint32)
+	buf.Next(4)
+
+	kv.val = buf.Bytes()
+}
+
+// Encode kv to bytes.
+func (kv *KV) Bytes() ([]byte, error) {
+	buf := new(bytes.Buffer)
+
+	// Encode size. We need this in case we will have to re-create
+	// keys without indexes.
+	err := binary.Write(buf, binary.BigEndian, uint32(len(kv.val)))
+	if err != nil {
+		return nil, err
+	}
+
+	// Write value.
+	_, err = buf.Write(kv.val)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
