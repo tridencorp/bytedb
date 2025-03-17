ac37200cde3db36e224516df05f3a5d36eb4ab1d
damianu <damian@datagentleman.io>
2025-03-17 19:42:40 +0100

    Add assert test helpers

---
+++ tests/assert.go
@@ -0,0 +1,22 @@
+package tests
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Assert[T comparable](t *testing.T, expected, actual T) {
+	t.Helper()
+	
+	if expected != actual { 
+		t.Errorf("Assertion failed: expected %v, got %v", expected, actual)
+	}
+}
+
+func AssertEqual[T any](t *testing.T, expected, actual T) {
+	t.Helper()
+	
+	if reflect.DeepEqual(expected, actual) { 
+		t.Errorf("Assertion failed: expected %v, got %v", expected, actual)
+	}
+}
