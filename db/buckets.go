7b7fe2fd12b22f98fc55dc8313be9d86a7e842d9
damianu <damian@datagentleman.io>
2025-03-17 20:52:23 +0100

    Buckets items and refCount

--- db/buckets.go
+++ db/buckets.go
@@ -2,14 +2,25 @@ package db
 
 import "sync/atomic"
 
+type item struct {
+	bucket   *Bucket
+	refCount atomic.Int32
+}
+
+func Item(b *Bucket) *item {
+	i := &item{bucket: b}
+	i.refCount.Store(1)
+	return i
+}
+
 type Buckets struct {
-	items map[uint32]*Bucket
+	items map[uint32]*item
 
 	// Max opened buckets at a time.
 	MaxOpened int16
 
 	// The latest bucket is special, all new keys go into it.
-	latest atomic.Pointer[Bucket]
+	latest atomic.Pointer[item]
 }
 
 // Open latest bucket.
@@ -19,8 +30,18 @@ func OpenBuckets(root string, maxFiles int16, conf Config) (*Buckets, error) {
 		return nil, err
 	}
 
-	buckets := &Buckets{ MaxOpened: maxFiles }
-	buckets.latest.Store(bucket)
+	buckets := &Buckets{ MaxOpened: maxFiles, items: map[uint32]*item{} }
+	item := Item(bucket)
+
+	buckets.latest.Store(item)
+	buckets.items[bucket.ID] = item
 
 	return buckets, nil
+}
+
+func (b *Buckets) Latest() *Bucket {
+	latest := b.latest.Load()
+	latest.refCount.Add(1)
+
+	return latest.bucket
 }
