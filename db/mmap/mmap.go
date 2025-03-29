ce62fddf4148981d36d1cf0f3fb6fe9dbca4e955
damianu <damian@datagentleman.io>
2025-03-29 21:47:55 +0100

    Open mmap

--- db/mmap/mmap.go
+++ db/mmap/mmap.go
@@ -1,9 +1,28 @@
 package mmap
 
-import "os"
+import (
+	"os"
+
+	"golang.org/x/sys/unix"
+)
 
 type Mmap struct {
 	file   *os.File
 	data   []byte
 	offset uint64
+}
+
+// Mmap file.
+func Open(file *os.File, size, flags int) (*Mmap, error) {
+	if flags == 0 {
+		flags = unix.PROT_READ | unix.PROT_WRITE
+	}
+
+	data, err := unix.Mmap(int(file.Fd()), 0, size, flags, unix.MAP_SHARED)
+	if err != nil {
+		return nil, err
+	}
+
+	mmap := &Mmap{file: file, data: data, offset: 0}
+	return mmap, nil
 }
---
+++ db/mmap/mmap_test.go
@@ -0,0 +1,20 @@
+package mmap
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestOpen(t *testing.T) {
+	file, _ := os.OpenFile("./test.wal", os.O_RDWR | os.O_CREATE, 0644)
+	
+	// Truncate in MB.
+	size := int64(1024 * 1024 * 1)
+	file.Truncate(size)
+
+	_, err := Open(file, int(size), 0)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
