d93e8bc231a0970c8027e7bd06a866f83d0b3638
damianu <damian@datagentleman.io>
2025-04-18 22:00:48 +0200

    Extract and redefine collisions

---
+++ db/index/collisions.go
@@ -0,0 +1,11 @@
+package index
+
+// Keeping track of key collisions
+type Collisions struct {
+	Keys []Key
+}
+
+// Open collisions file. Create one if it doesn't exists.
+func OpenCollisions() {
+
+}
