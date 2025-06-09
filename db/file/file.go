84c58916f8b962a69e6f43515956daedc4c38c4b
damianu <damian@datagentleman.io>
2025-06-09 21:10:06 +0200

    Resize file

--- db/file/file.go
+++ db/file/file.go
@@ -4,7 +4,8 @@ import "os"
 
 // File structure that manages data in blocks.
 type File struct {
-	file *os.File
+	file      *os.File
+	blockSize uint16
 }
 
 func Open(path string) (*File, error) {
@@ -13,5 +14,20 @@ func Open(path string) (*File, error) {
 		return nil, nil
 	}
 
-	return &File{file: file}, nil
+	return &File{file: file, blockSize: 4096}, nil
+}
+
+// Resize file to given size.
+func (f *File) Resize(size int64) error {
+	err := f.file.Truncate(size)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Write data to given block number.
+func (f *File) WriteBlock(num int, data []byte) error {
+	return nil
 }
