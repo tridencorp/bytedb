4b2dcd206624172925f7e81adf660b9d302c8119
damianu <damian@datagentleman.io>
2025-06-19 17:40:50 +0200

    Get file for given id

--- db/directory.go
+++ db/directory.go
@@ -1,12 +1,29 @@
 package db
 
-// Manage files and subdirectories.
+import (
+	"fmt"
+	"os"
+)
+
+// Manage files and subdirectories.s
 type Directory struct {
-	Dir    string
+	Root   string
+	Ext    string
 	PerDir int
-	Files  []File
 }
 
-func OpenDirectory(dir string, perDir int) *Directory {
-	return &Directory{Dir: dir, PerDir: perDir}
+func OpenDirectory(root string, perDir int, extension string) *Directory {
+	return &Directory{Root: root, PerDir: perDir, Ext: extension}
+}
+
+// Get file from directory. Create it if it doesn't already exist.
+func (d *Directory) Get(id int) (*File, error) {
+	// Get subdir based on id using ceil technique.
+	subdir := (d.PerDir + id - 1) / d.PerDir
+
+	// Build path for given id:
+	// 	* root/subdir/id.ext
+	path := fmt.Sprintf("%s/%d/%d.%s", d.Root, subdir, id, d.Ext)
+
+	return OpenPath(path, os.O_RDWR|os.O_CREATE)
 }
