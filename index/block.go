5c3c894e57d3f9aa2c4418228302db9894f8ff2d
damianu <damian@datagentleman.io>
2025-09-05 20:44:32 +0200

    Write data to block

--- index/block.go
+++ index/block.go
@@ -1,11 +1,31 @@
 package index
 
+import (
+	"unsafe"
+)
+
+const BlockSize = 4096
+const HeaderSize = int(unsafe.Sizeof(Header{}))
+const DataSize = BlockSize - HeaderSize
+
 type Header struct {
 	Offset uint32
 }
 
 type Block struct {
-	Header []byte
-	Data   []byte
-	Size   uint32
+	Header Header
+	Data   [DataSize]byte
+}
+
+// Write bytes to block and return the number of bytes written
+func (b *Block) Write(data []byte) int {
+	// Check if block has enough space
+	if int(b.Header.Offset)+len(data) > cap(b.Data) {
+		return 0
+	}
+
+	n := copy(b.Data[b.Header.Offset:], data)
+	b.Header.Offset += uint32(n)
+
+	return n
 }
---
+++ index/block_test.go
@@ -0,0 +1,11 @@
+package index
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFooterRead(t *testing.T) {
+	fmt.Println("x: ", HeaderSize)
+	fmt.Println("x: ", DataSize)
+}
