62536c6991bde2a33a9f58414df20a2a8d0b10f9
damianu <damian@datagentleman.io>
2025-11-07 23:28:24 +0100

    Start listening on socket

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,32 @@ package main
 import (
 	"bytedb/server"
 	"log"
+	"syscall"
 )
 
 func main() {
-	// Start database server
 	log.Println("Starting database server...")
-	server.Run([4]byte{127, 0, 0, 1}, 6666)
+
+	// Create database server
+	fd := server.Run([4]byte{127, 0, 0, 1}, 6666)
+
+	// Close listening socket
+	defer syscall.Close(fd)
+
+	// Accept connections
+	for {
+		nfd, addr, err := syscall.Accept(fd)
+		if err != nil {
+			log.Println("accept error:", err)
+			continue
+		}
+
+		// For this version each connection will be run in separate goroutine.
+		// Later we will use poll/epoll together with goroutine pool.
+		go handleConn(nfd, addr)
+	}
+}
+
+func handleConn(nfd int, addr syscall.Sockaddr) {
+	log.Println("handling connection...")
 }
--- server/server.go
+++ server/server.go
@@ -6,16 +6,18 @@ import (
 )
 
 // Run TCP server
-func Run(address [4]byte, port int) *syscall.SockaddrInet4 {
+func Run(address [4]byte, port int) int {
 	// Create socket
 	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, syscall.IPPROTO_TCP)
 	if err != nil {
 		log.Fatalf("socket: %v", err)
+		return -1
 	}
 
 	// Enable SO_REUSEADDR
 	if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err != nil {
 		log.Fatal("setsockopt:", err)
+		return -1
 	}
 
 	// Set port and address
@@ -27,7 +29,14 @@ func Run(address [4]byte, port int) *syscall.SockaddrInet4 {
 	// Bind socket
 	if err := syscall.Bind(fd, s); err != nil {
 		log.Fatalf("bind: %v", err)
+		return -1
 	}
 
-	return s
+	// Listen on socket
+	if err := syscall.Listen(fd, syscall.SOMAXCONN); err != nil {
+		syscall.Close(fd)
+		return -1
+	}
+
+	return fd
 }
