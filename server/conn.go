be628e4e4a337f23478a9dcd0d0ca487f5d8bc89
damianu <damian@datagentleman.io>
2025-11-11 18:14:18 +0100

    Read full msg

--- server/conn.go
+++ server/conn.go
@@ -1,12 +1,17 @@
 package server
 
 import (
+	"bytedb/common"
+	"fmt"
+	"io"
 	"net"
 	"strconv"
 	"strings"
 	"sync"
 	"syscall"
 )
+
+const PrefixLen = 4
 
 // Wrapper for user connection
 type Conn struct {
@@ -20,8 +25,11 @@ func Connect(address string) (*Conn, error) {
 	parts := strings.Split(address, ":")
 
 	// Set addr and port
-	ip      := net.ParseIP(parts[0])
-	port, _ := strconv.Atoi(parts[1])
+	ip := net.ParseIP(parts[0])
+	port, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return nil, err
+	}
 
 	addr := &syscall.SockaddrInet4{
 		Port: port,
@@ -49,12 +57,50 @@ func NewConn(fd int) *Conn {
 	return &Conn{fd: fd}
 }
 
-// Read from connection. It will block untill all data is read.
-func (c *Conn) Read(buf []byte) (int, error) {
-	return syscall.Read(c.fd, buf)
+// Read from connection, blocking until whole command is read
+func (c *Conn) Read() ([]byte, error) {
+	buf := make([]byte, 1024)
+	size := uint32(0)
+
+	// 1. Fast path - try to read all at once
+	n, err := syscall.Read(c.fd, buf)
+	if err != nil {
+		return nil, err
+	}
+
+	if n < PrefixLen {
+		return nil, fmt.Errorf("short read: got %d bytes", n)
+	}
+
+	// Decode msg size
+	common.Decode(buf[:PrefixLen], &size)
+
+	// Check if we get all data in one read
+	if uint32(n-PrefixLen) == size {
+		return buf[PrefixLen:n], nil
+	}
+
+	// 2. Didn't get all data, we need to read remaining bytes
+	total := make([]byte, 0, size)
+	total = append(total, buf[PrefixLen:n]...)
+
+	for uint32(len(total)) < size {
+		n, err = syscall.Read(c.fd, buf)
+		if err != nil {
+			return nil, err
+		}
+
+		if n == 0 {
+			return nil, io.ErrUnexpectedEOF // connection closed
+		}
+
+		total = append(total, buf[:n]...)
+	}
+
+	return total, nil
 }
 
-// Write to connection. It will block untill all data is wrote.
+// Write data to connection, blocking until done
 func (c *Conn) Write(buf []byte) (int, error) {
 	c.mux.Lock()
 	defer c.mux.Unlock()
