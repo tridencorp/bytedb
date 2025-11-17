f6bdc108f41be64b9e2a61f96a77084d8f4a1439
damianu <damian@datagentleman.io>
2025-11-18 00:22:08 +0100

    Create index package

--- index/index.go
+++ index/index.go
@@ -1,42 +1,8 @@
 package index
 
-import (
-	"bytedb/db/mmap"
-	"hash/fnv"
-	"os"
-)
-
-type IndexFile struct {
-	len       uint64
-	maxBlocks uint64
-	file      *os.File
-	blocks    *mmap.Mmap
-	growBy    uint64 // blocks added per each resize
-}
-
-// Open mmaps the given file and returns an IndexFile.
-// blockCount specifies how many blocks will be mapped from the file into memory.
-func Open(file *os.File, blockCount uint64) (*IndexFile, error) {
-	blocks, err := mmap.Open(file, int(blockCount), 0)
-	if err != nil {
-		return nil, err
-	}
-
-	return &IndexFile{maxBlocks: blockCount, blocks: blocks, growBy: blockCount}, nil
-}
-
-// Write writes index key to file
-func (f *IndexFile) Write(key []byte) {
-	// Calculate block number starting from 1 (0 is reserved for file header)
-	// n := (hash(key) % f.maxBlocks) + 1
-
-	// Get block
-}
-
-// hash calculates FNV 64-bit hash of the given key
-func hash(key []byte) uint64 {
-	h := fnv.New64a()
-	h.Write([]byte(key))
-
-	return h.Sum64()
+// DataClass
+type Index struct {
+	Hash   uint64
+	Offset uint32
+	Len    uint32
 }
--- index/index_test.go
+++ index/index_test.go
@@ -1,18 +1,8 @@
 package index
 
 import (
-	"bytedb/tests"
-	"fmt"
-	"os"
 	"testing"
 )
 
 func TestIndex(t *testing.T) {
-	f, _ := os.OpenFile("test.file", os.O_RDWR|os.O_CREATE, 0644)
-	defer os.Remove("test.file")
-
-	indexes, err := Open(f, 1)
-	tests.AssertEqual(t, err, nil)
-
-	fmt.Println(indexes.blocks)
 }
